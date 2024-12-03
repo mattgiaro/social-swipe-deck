132c8e045ba406a79071818470b83ca7a42df5cd
mattgiaro <131328870+mattgiaro@users.noreply.github.com>
2024-12-03 10:58:18 +0400

    favicon fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,15 +17,13 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.svg' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
     ],
     apple: [
       { url: '/apple-touch-icon.png' }
     ],
     other: [
-      { rel: 'android-chrome-192x192', url: '/android-chrome-192x192.png' },
-      { rel: 'android-chrome-512x512', url: '/android-chrome-512x512.png' },
       { rel: 'manifest', url: '/site.webmanifest' }
     ],
   },
